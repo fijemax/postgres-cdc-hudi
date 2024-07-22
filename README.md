729daec86b488c620b80fe0997951f7298bce91f
Léni Gauffier <lenigauffier@gmail.com>
2024-07-22 17:25:46 +0200

    feat: Update Hudi Spark services in docker-compose.yml

--- README.md
+++ README.md
@@ -21,6 +21,35 @@ This will load the necessary data into the database and create the Kafka Source 
 ./init.sh
 ```
 
+Create a `hudi` bucket in MinIO
+
+```bash
+docker compose exec minio mc mb data/hudi
+```
+
+Create an access key and secret key for the bucket
+
+```bash
+docker compose exec minio mc admin user add data/hudi hudi hudi
+```
+
+3. **Start the ingestion**
+
+```bash
+./scripts/spark-stream.sh
+```
+
+4. **Query the data**
+
+```bash
+docker compose exec spark-master spark-shell --packages org.apache.hudi:hudi-spark3.5-bundle_2.12:0.15.0 --properties-file hudi/spark-config.properties
+```
+
+```scala
+val basicDF = spark.read.format("hudi").load("s3a://hudi/basic")
+basicDF.show()
+```
+
 ## Services
 
 - [Control Center](http://localhost:9021)
@@ -31,7 +60,10 @@ This will load the necessary data into the database and create the Kafka Source 
 - [REST Proxy](http://localhost:8082)
 - [Spark](http://localhost:4041)
 
+## Query Hudi
+
+
 
 ## Warnings
 
-- Apicurio does not work with Hudi (at this time), so we're forced to use the Confluent Schema Registry.
+- Apicurio seems to not work with Hudi (at this time), so we're forced to use the Confluent Schema Registry.
--- scripts/spark-stream.sh
+++ scripts/spark-stream.sh
@@ -6,7 +6,7 @@ docker compose exec spark-master spark-submit \
 --executor-memory 1g \
 hudi/hudi.jar \
 --table-type COPY_ON_WRITE \
---target-base-path file:///usr/data/basic \
+--target-base-path s3a://hudi/basic \
 --target-table basic_table \
 --source-class org.apache.hudi.utilities.sources.debezium.PostgresDebeziumSource \
 --payload-class org.apache.hudi.common.model.debezium.PostgresDebeziumAvroPayload \
@@ -14,17 +14,5 @@ hudi/hudi.jar \
 --continuous \
 --source-limit 4000000 \
 --min-sync-interval-seconds 20 \
---hoodie-conf bootstrap.servers=kafka:29092 \
---hoodie-conf schema.registry.url=http://schema-registry:8081 \
---hoodie-conf hoodie.streamer.schemaprovider.registry.url=http://schema-registry:8081/subjects/cdc.public.basic-value/versions/latest \
---hoodie-conf hoodie.streamer.source.kafka.value.deserializer.class=io.confluent.kafka.serializers.KafkaAvroDeserializer \
---hoodie-conf hoodie.streamer.source.kafka.topic=cdc.public.basic \
---hoodie-conf auto.offset.reset=earliest \
---hoodie-conf hoodie.datasource.write.recordkey.field=id \
---hoodie-conf hoodie.datasource.write.partitionpath.field=name \
---hoodie-conf hoodie.datasource.write.keygenerator.class=org.apache.hudi.keygen.SimpleKeyGenerator
---hoodie-conf hoodie.datasource.write.hive_style_partitioning=true
---hoodie-conf hoodie.datasource.write.precombine.field=name
---source-ordering-field ts  \
-
-# --properties-file config/spark-config.properties \
+--source-ordering-field timestamp  \
+--props hudi/hudi.properties
